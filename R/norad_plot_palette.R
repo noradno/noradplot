e64c0424064aa5406e3c82f4fbea7b49200f5721
einartornes <45436606+einartornes@users.noreply.github.com>
2022-10-11 22:45:08 +0200

    new palettes

--- R/norad_plot_palette.R
+++ R/norad_plot_palette.R
@@ -1,14 +1,14 @@
 #' Norad colors
 norad_colors <- c(
-  "green" = "#23716D",
-  "blue" = "#437793",
-  "red" = "#9E5B4C",
-  "yellow" = "#C1872E",
   "lightgreen" = "#8AACA1",
-  "lightblue" = "#86AAC2",
-  "lightred" = "#C6836D",
   "lightyellow" = "#E0B052",
+  "green" = "#23716D",
+  "yellow" = "#C1872E",
   "black" = "#353533",
+  "lightblue" = "#86AAC2",
+  "blue" = "#437793",
+  "lightred" = "#C6836D",
+  "red" = "#9E5B4C",
   "grey" = "#B1ACA2")
 
 #' Function to extract Norad colors as hex codes
@@ -27,7 +27,28 @@ norad_cols <- function(...) {
 
 #' Combine colors into palettes
 norad_palettes <- list(
-  "main"  = norad_cols("green", "blue", "red", "yellow"),
+  "green"   = norad_cols("lightgreen"),
+  "greens"   = norad_cols("lightgreen", "green"),
+  "yellow"   = norad_cols("lightyellow"),
+  "yellows"   = norad_cols("lightyellow", "yellow"),
+  "blue"   = norad_cols("lightblue"),
+  "blues"   = norad_cols("lightblue", "blues"),
+  "red"   = norad_cols("lightred"),
+  "reds"   = norad_cols("lightred", "red"),
+  "dark"   = norad_cols("black"),
+  "darks"   = norad_cols("black", "grey"),
+
+  "pal1"   = norad_cols("lightgreen"),
+  "pal2"   = norad_cols("lightgreen", "lightyellow"),
+  "pal3"   = norad_cols("lightgreen", "lightyellow", "green"),
+  "pal4"   = norad_cols("lightgreen", "lightyellow", "green", "black"),
+  "pal5"   = norad_cols("lightgreen", "lightyellow", "green", "yellow", "black"),
+  "pal6"   = norad_cols("lightgreen", "lightyellow", "green", "yellow", "lightblue", "black"),
+  "pal7"   = norad_cols("lightgreen", "lightyellow", "green", "yellow", "lightblue", "blue", "black"),
+  "pal8"   = norad_cols("lightgreen", "lightyellow", "green", "yellow", "lightblue", "blue", "lightred", "black"),
+  "pal9"   = norad_cols("lightgreen", "lightyellow", "green", "yellow", "lightblue", "blue", "lightred", "red", "black"),
+  "pal10"   = norad_cols("lightgreen", "lightyellow", "green", "yellow", "lightblue", "blue", "lightred", "red", "grey", "black"),
+
   "full"  = norad_cols())
 
 #' Return function to interpolate a Norad color palette
@@ -37,7 +58,7 @@ norad_palettes <- list(
 #' @param ... Additional arguments to pass to colorRampPalette()
 #'
 
-norad_pal <- function(palette = "main", reverse = FALSE, ...) {
+norad_pal <- function(palette = "pal10", reverse = FALSE, ...) {
   pal <- norad_palettes[[palette]]
 
   if (reverse)
@@ -55,7 +76,7 @@ norad_pal <- function(palette = "main", reverse = FALSE, ...) {
 #'            scale_color_gradientn(), used respectively when discrete is TRUE or FALSE
 #'
 
-scale_color_norad <- function(palette = "main", discrete = TRUE, reverse = FALSE, ...) {
+scale_color_norad <- function(palette = "pal10", discrete = TRUE, reverse = FALSE, ...) {
   pal <- norad_pal(palette = palette, reverse = reverse)
 
   if (discrete) {
@@ -74,7 +95,7 @@ scale_color_norad <- function(palette = "main", discrete = TRUE, reverse = FALSE
 #'            scale_fill_gradientn(), used respectively when discrete is TRUE or FALSE
 #'
 
-scale_fill_norad <- function(palette = "main", discrete = TRUE, reverse = FALSE, ...) {
+scale_fill_norad <- function(palette = "pal10", discrete = TRUE, reverse = FALSE, ...) {
   pal <- norad_pal(palette = palette, reverse = reverse)
 
   if (discrete) {
