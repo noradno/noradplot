ccab159ed4a0e0892b933faf579fddefc51b745d
Eivind Moe Hammersmark <eivindhammers@gmail.com>
2023-02-21 12:24:46 +0100

    update README, new style, sticker.

--- R/style_functions.R
+++ R/style_functions.R
@@ -1,33 +1,61 @@
 #' Function for using Norad's theme in ggplot2 graphics
 #'
+#'
+
+sysfonts::font_add_google(name = "Arimo")
+sysfonts::font_add_google(name = "EB Garamond")
+
+ggplot2::update_geom_defaults("path", list(linewidth = 1))
 
 theme_norad <- function() {
-  font <- "sans"
+  sans <- "Arimo"
+  serif <- "EB Garamond"
 
-  ggplot2::theme_minimal() +
-  ggplot2::theme(
-    plot.title = ggplot2::element_text(
-      family = font,
-      size = 22,
-      face = "bold",
-      color = "black",
-      hjust = 0.5),
-    plot.subtitle = ggplot2::element_text(
-      family = font,
-      size = 12,
-      hjust = 0.5),
-    plot.caption = ggplot2::element_text(
-      family = font,
-      size = 10),
-    legend.position = "bottom",
-    legend.text.align = 0,
-    legend.background = ggplot2::element_blank(),
-    legend.title = ggplot2::element_blank(),
-    axis.title = ggplot2::element_blank(),
-    axis.text = ggplot2::element_text(
-      size = 10,
-      face = "bold",
-      color = "black"
+  ggplot2::theme_classic() +
+    ggplot2::theme(
+      # General text
+      text = ggplot2::element_text(
+        family = sans, # default unless otherwise stated (like in plot.title)
+        size = 12
+      ),
+      # Plot title and subtitle
+      plot.title.position = "plot",
+      plot.title = ggplot2::element_text(
+        family = serif,
+        size = 16,
+        face = "bold",
+        hjust = 0),
+      plot.subtitle = ggplot2::element_text(
+        size = 14,
+        hjust = 0,
+        margin = margin(t = 5, b = 5)),
+      # Plot caption
+      plot.caption.position = "plot",
+      plot.caption = ggplot2::element_text(
+        size = 12,
+        hjust = 0,
+        margin = margin(t = 10)),
+      # Legend
+      legend.text = ggplot2::element_text(
+        size = 12),
+      legend.title = ggplot2::element_text(
+        size = 12),
+      legend.position = "right",
+      legend.justification = "top",
+      legend.margin = margin(r = 30),
+      legend.text.align = 0,
+      legend.background = ggplot2::element_blank(),
+      # Axes
+      axis.text = ggplot2::element_text(
+        size = 12,
+        color = "black"
+      ),
+      axis.title = ggplot2::element_text(
+        size = 12
+      ),
+      axis.title.y = ggplot2::element_text(margin = margin(r = 5)),
+      axis.title.x = ggplot2::element_text(margin = margin(t = 10)),
+      axis.text.y = ggplot2::element_text(margin = margin(r = 5)),
+      axis.text.x = ggplot2::element_text(margin = margin(t = 10))
     )
-  )
 }
--- README.md
+++ README.md
@@ -1,4 +1,28 @@
-# noradplot
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# noradplot <img src="man/figures/sticker.png" align="right" width="120" />
+
+## Overview
+
 Norads colour palette and plot style for creating ggplot2 graphics.
 
-Credits: Based on Simon Jackson's blog post: Creating corporate colour palettes for ggplot2, BBC BBC Visual and Data Journalism cookbook for R graphics: https://bbc.github.io/rcookbook/ and Meghan Halls blog post Creating Custom colour palettes for ggplot2: https://meghan.rbind.io/blog/2022-10-11-creating-custom-color-palettes-with-ggplot2/
+## Example
+
+``` r
+
+library(ggplot2)
+library(noradplot)
+ggplot(mpg, aes(displ, hwy, color = class)) + 
+  geom_point() +
+  theme_norad() +
+  scale_color_norad()
+```
+
+![](man/figures/README-example-1.png)<!-- -->
+
+Credits: Based on Simon Jackson’s blog post: Creating corporate colour
+palettes for ggplot2, BBC BBC Visual and Data Journalism cookbook for R
+graphics: <https://bbc.github.io/rcookbook/> and Meghan Halls blog post
+Creating Custom colour palettes for ggplot2:
+<https://meghan.rbind.io/blog/2022-10-11-creating-custom-color-palettes-with-ggplot2/>
