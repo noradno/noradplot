d050f056eb6974fe2c0e80392a21f28fbd375d40
Eivind Moe Hammersmark <eivindhammers@gmail.com>
2023-12-18 15:25:12 +0100

    Add simplified world map

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' World map of sovereign states
+#'
+#' World map of sovereign states based on Natural Earth data, modified to include Gaza and the West Bank as separate entities.
+#' Also includes a few other minor modifications, like the inclusion of Abyie on the Sudan-South Sudan border,
+#' separating Western Sahara from Morocco, and merging of North and South Cyprus.
+#'
+#' @format ## `wrld`
+#' Simple feature collection with 200 features and 168 fields
+#' \describe{
+#'   ...
+#' }
+#' @source <https://www.naturalearthdata.com/download/50m/cultural/ne_50m_admin_0_sovereignty.zip>
+"wrld"
